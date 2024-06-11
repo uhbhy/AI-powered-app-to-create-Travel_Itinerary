a1b78a6c2df8186d81b46a362220f2d2d870bda7
uhbhy <109104576+uhbhy@users.noreply.github.com>
2024-06-11 14:36:12 +0530

    Added additional pages

---
+++ Contact_Us.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def contact_us_page():
+    title_template = """
+    <div style="background-color:#333333; padding:5px;">
+    <h1 style="color:cyan">GLOBE GURU</h1>
+    </div>
+    """
+    st.sidebar.subheader("MENU")
+    st.sidebar.image("logo.jpg",use_column_width=True)
+    st.markdown(title_template,unsafe_allow_html=True)
+    st.subheader("Contact Us")
+    st.write("""
+    Have questions, feedback, or need assistance? We're here to help! Reach out to us through any of the following channels:
+
+    **Email:** abhirupbasu30@gmail.com
+    **Phone:** +91 8235026220
+
+    Our dedicated team is committed to providing prompt and helpful assistance to all our users. We pride ourselves on our speedy responses and our ability to resolve issues as soon as possible.
+
+    Your feedback is invaluable to us. If you encounter any issues or have suggestions for improving the app, please don't hesitate to report them to us. By working together, we can make Globe Guru even better and ensure a seamless user experience for everyone.
+
+    Thank you for choosing Globe Guru! We look forward to hearing from you and assisting you on your travel journey.
+    """)
+
+if __name__ == "__main__":
+    contact_us_page()
